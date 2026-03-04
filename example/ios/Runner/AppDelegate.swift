b3da24a11873b2a5e54975b1a61ad43ba9e1fd91
Muhammad Bani Akram <baniakram96@hotmail.com>
2026-03-04 11:07:49 +0500

    updated readme file, license and pubspec.yaml

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -3,11 +3,23 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    // Handle Universal Link redirects
+
+    override func application(_ app: UIApplication,
+    openurl: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+
+        // Let Braintree decide if this belongs to Venmo / PayPal / etc
+        if BTAppContextSwitcher.sharedInstance.handleOpen(url) {
+            return true
+        }
+
+        return super.application(app, open: url, options: options)
+    }
 }
